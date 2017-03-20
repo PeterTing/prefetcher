39716549d96287fc30a1e6a8f95d66517ca4449c
PeterTing <a0917209079@gmail.com>
2017-03-20 23:22:22 +0800

    Add avx implementation
    Add avx method implementation into impl.c.

--- Makefile
+++ Makefile
@@ -1,21 +1,23 @@
-EXEC = time_test_naive time_test_sse time_test_sse_prefetch test
+EXEC = time_test_naive time_test_sse time_test_sse_prefetch time_test_avx
 
-CFLAGS = -msse2 --std gnu99 -O0 -Wall -Wextra -g
+CFLAGS = -mavx2 -msse2 --std gnu99 -O0 -Wall -Wextra -g
+
+#DEFINE = -DNAIVE -DSSE -DSSEPREFETCH
 
 GIT_HOOKS := .git/hooks/applied
 
-default: $(GIT_HOOKS) impl_test.o
-	$(CC) $(CFLAGS) impl_test.o main_test.c -o test
-#	$(CC) $(CFLAGS) -o time_test_naive -DNAIVE main.c
-#	$(CC) $(CFLAGS) -o time_test_sse -DSSE main.c
-#	$(CC) $(CFLAGS) -o time_test_sse_prefetch -DSSEPREFETCH main.c
-
-%.o: %.c
-	$(CC) -c $(CFALGS) $< -o $@
+default: $(GIT_HOOKS) main.c
+	$(CC) $(CFLAGS) -o time_test_naive -DNAIVE main.c
+	$(CC) $(CFLAGS) -o time_test_sse -DSSE main.c
+	$(CC) $(CFLAGS) -o time_test_sse_prefetch -DSSEPREFETCH main.c
+	$(CC) $(CFLAGS) -o time_test_avx -DAVX main.c
+	
+#%.o: %.c
+#	$(CC) -c $(CFALGS) $< -o $@
 
 $(GIT_HOOKS):
 	@scripts/install-git-hooks
 	@echo
 
 clean:
-	$(RM) $(EXEC) main 
+	$(RM) $(EXEC) 
--- main.c
+++ main.c
@@ -5,10 +5,13 @@
 #include <string.h>
 #include <assert.h>
 
+#include <immintrin.h>
 #include <xmmintrin.h>
 
 #define TEST_W 4096
 #define TEST_H 4096
+
+
 
 /* provide the implementations of naive_transpose,
  * sse_transpose, sse_prefetch_transpose
@@ -29,65 +32,75 @@ static long diff_in_us(struct timespec t1, struct timespec t2)
     return (diff.tv_sec * 1000000.0 + diff.tv_nsec / 1000.0);
 }
 
+char *name;
+void (*transposeMethod)(int *src, int *dst, int w, int h);
 int main()
 {
+#if defined(NAIVE)
+    transposeMethod = naive_transpose;
+    name = "naive";
+#elif(SSE)
+    transposeMethod = sse_transpose;
+    name = "sse";
+#elif(AVX)
+    transposeMethod = avx_transpose;
+    name = "avx";
+#else
+    transposeMethod = sse_prefetch_transpose;
+    name = "sse_prefetch";
+#endif
     /* verify the result of 4x4 matrix */
     {
-        int testin[16] = { 0, 1,  2,  3,  4,  5,  6,  7,
-                           8, 9, 10, 11, 12, 13, 14, 15
+        int testin[64] = { 0, 1,  2,  3,  4,  5,  6,  7,
+                           8, 9, 10, 11, 12, 13, 14, 15,
+                           16, 17, 18, 19, 20, 21, 22, 23,
+                           24, 25, 26, 27, 28, 29, 30, 31,
+                           32, 33, 34, 35, 36, 37, 38, 39,
+                           40, 41, 42, 43, 44, 45, 46, 47,
+                           48, 49, 50, 51, 52, 53, 54, 55,
+                           56, 57, 58, 59, 60, 61, 62, 63
                          };
-        int testout[16];
-        int expected[16] = { 0, 4,  8, 12, 1, 5,  9, 13,
-                             2, 6, 10, 14, 3, 7, 11, 15
+        int testout[64];
+        int expected[64] = { 0, 8, 16, 24, 32, 40, 48, 56,
+                             1, 9, 17, 25, 33, 41, 49, 57,
+                             2, 10, 18, 26, 34, 42, 50, 58,
+                             3, 11, 19, 27, 35, 43, 51, 59,                                                    4, 12, 20, 28, 36, 44, 52, 60,
+                             5, 13, 21, 29, 37, 45, 53, 61,
+                             6, 14, 22, 30, 38, 46, 54, 62,
+                             7, 15, 23, 31, 39, 47, 55, 63
                            };
 
-        for (int y = 0; y < 4; y++) {
-            for (int x = 0; x < 4; x++)
-                printf(" %2d", testin[y * 4 + x]);
+        for (int y = 0; y < 8; y++) {
+            for (int x = 0; x < 8; x++)
+                printf(" %2d", testin[y * 8 + x]);
             printf("\n");
         }
         printf("\n");
-        sse_transpose(testin, testout, 4, 4);
-        for (int y = 0; y < 4; y++) {
-            for (int x = 0; x < 4; x++)
-                printf(" %2d", testout[y * 4 + x]);
+        transposeMethod(testin, testout, 8, 8);
+        for (int y = 0; y < 8; y++) {
+            for (int x = 0; x < 8; x++)
+                printf(" %2d", testout[y * 8 + x]);
             printf("\n");
         }
-        assert(0 == memcmp(testout, expected, 16 * sizeof(int)) &&
+        assert(0 == memcmp(testout, expected, 64 * sizeof(int)) &&
                "Verification fails");
     }
 
     {
         struct timespec start, end;
         int *src  = (int *) malloc(sizeof(int) * TEST_W * TEST_H);
-        int *out0 = (int *) malloc(sizeof(int) * TEST_W * TEST_H);
-        int *out1 = (int *) malloc(sizeof(int) * TEST_W * TEST_H);
-        int *out2 = (int *) malloc(sizeof(int) * TEST_W * TEST_H);
+        int *out =  (int *) malloc(sizeof(int) * TEST_W * TEST_H);
 
         srand(time(NULL));
         for (int y = 0; y < TEST_H; y++)
             for (int x = 0; x < TEST_W; x++)
                 *(src + y * TEST_W + x) = rand();
-
         clock_gettime(CLOCK_REALTIME, &start);
-        sse_prefetch_transpose(src, out0, TEST_W, TEST_H);
+        transposeMethod(src, out, TEST_W, TEST_H);
         clock_gettime(CLOCK_REALTIME, &end);
-        printf("sse prefetch: \t %ld us\n", diff_in_us(start, end));
-
-        clock_gettime(CLOCK_REALTIME, &start);
-        sse_transpose(src, out1, TEST_W, TEST_H);
-        clock_gettime(CLOCK_REALTIME, &end);
-        printf("sse: \t\t %ld us\n", diff_in_us(start, end));
-
-        clock_gettime(CLOCK_REALTIME, &start);
-        naive_transpose(src, out2, TEST_W, TEST_H);
-        clock_gettime(CLOCK_REALTIME, &end);
-        printf("naive: \t\t %ld us\n", diff_in_us(start, end));
-
+        printf("%s \t %ld us\n", name, diff_in_us(start, end));
         free(src);
-        free(out0);
-        free(out1);
-        free(out2);
+        free(out);
     }
 
     return 0;
